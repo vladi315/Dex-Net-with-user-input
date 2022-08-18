9a560df30a0481a46d3da466a2beb6567c2d8c44
vladislav <klass.vladislav.ext@siemens.com>
2022-08-18 19:55:29 +0200

    Feature: transform grasp to synthetic object frame

--- fmp-tools/fmp_thesis_evaluation_policy_klass.py
+++ fmp-tools/fmp_thesis_evaluation_policy_klass.py
@@ -37,6 +37,7 @@ import argparse
 import json
 import os
 import time
+from operator import matmul
 
 import cv2
 import numpy as np
@@ -49,6 +50,7 @@ from gqcnn.grasping import (CrossEntropyRobustGraspingPolicy,
 from gqcnn.grasping.policy.policy import (RgbdImageState,
                                           RgbdImageStateWithUserInput)
 from gqcnn.utils import GripperMode
+from transformations import concatenate_matrices, rotation_matrix
 from visualization import Visualizer2D as vis
 
 from fmp_png_to_npy_converter import (convert_depth_to_dexnet_format,
@@ -68,7 +70,7 @@ def run_dex_net(model_name,
         config_filename = None, 
         fully_conv = None, 
         camera_pose_path = None, 
-        user_input_3d_folder = None, 
+        user_input_3d_dir = None, 
         user_input_fusion_method = None, 
         user_input_weight = None,
         user_input_point_number = 0 # this variable controls which user input point is loaded during the evaluation
@@ -76,10 +78,8 @@ def run_dex_net(model_name,
     assert not (fully_conv and depth_im_filename is not None
                 and segmask_filename is None
                 ), "Fully-Convolutional policy expects a segmask."
-    assert not (user_input_3d_folder is None and (user_input_fusion_method is not None or user_input_weight is not None)
-                ), "If you provide user_input_fusion method or user_input_weight, you also must provide user_input_3d_folder."
-    assert (user_input_weight == "zero" or user_input_weight == "very low" or user_input_weight == "low" or user_input_weight == "medium" or user_input_weight == "high" or user_input_weight == "very high" 
-                ), "If you provide user_input_fusion method or user_input_weight, you also must provide user_input_3d_folder."
+    assert not (user_input_3d_dir is None and (user_input_fusion_method is not None or user_input_weight is not None)
+                ), "If you provide user_input_fusion method or user_input_weight, you also must provide user_input_3d_dir."
     assert not (depth_ims_dir and depth_im_filename
             # TODO: remove one parameter and detect whether a filename or directory is provided
                 ), "Provide either a depth-ims_dir or a depth im_filename, but not both."
@@ -245,14 +245,24 @@ def run_dex_net(model_name,
                 vis.imshow(segmask)
             vis.show()
 
-        if user_input_3d_folder is not None:
+        # TODO: delete
+        # if user_input_3d_dir is not None:
+        #     # Project 3d user input into image plane
+        #     user_input_point_2d = project_user_input_to_image(camera_poses[depth_im_idx], user_input_3d_dir, camera_intr.K, camera_intr.height, camera_intr.width)
+        #     # get the right point
+        #     user_input_point_2d = user_input_point_2d[user_input_point_number:user_input_point_number+1]
+        # else: 
+        #     user_input_point_2d = None
+
+        if user_input_3d_dir is not None:
             # Project 3d user input into image plane
-            user_input_point_2d = project_user_input_to_image(camera_poses[depth_im_idx], user_input_3d_folder, camera_intr.K, camera_intr.height, camera_intr.width)
-            # use only first point 
+            user_input_point_2d = project_user_input_to_image(camera_poses[depth_im_idx], user_input_3d_dir, camera_intr.K, camera_intr.height, camera_intr.width)
+            # get the right point
             user_input_point_2d = user_input_point_2d[user_input_point_number:user_input_point_number+1]
 
         # Optionally read a segmask.
         segmask = None
+
         if user_input_fusion_method == "masking":
             if segmask_filename is not None:
                 # combine two segmentation masks (one from object segmentation, one from user input)
@@ -260,6 +270,7 @@ def run_dex_net(model_name,
                 segmask_user_input = cv2.imread(segmask_user_input_filename, cv2.IMREAD_GRAYSCALE)
                 segmask = cv2.imread(segmask_filename, cv2.IMREAD_GRAYSCALE)
 
+                # combine two segmentation masks (one from object segmentation, one from user input)
                 # Binarize both segmasks
                 thresh = 127
                 segmask_user_input = cv2.threshold(segmask_user_input, thresh, 255, cv2.THRESH_BINARY)[1]
@@ -291,7 +302,7 @@ def run_dex_net(model_name,
         # Create state.
         rgbd_im = RgbdImage.from_color_and_depth(color_im, depth_im)
         
-        if user_input_3d_folder is None: 
+        if user_input_3d_dir is None: 
             state = RgbdImageState(rgbd_im, camera_intr, segmask) 
             
         else:            
@@ -318,13 +329,9 @@ def run_dex_net(model_name,
         policy_start = time.time()
         action = policy(state)
 
-        if user_input_3d_folder is not None:            
-            mean_evaluation_metric, grasp_quality, distance_grasp_to_user_input_norm = calc_evaluation_metrics(
-                action, policy, user_input_point_2d, state.camera_intr)
-        else:
-            mean_evaluation_metric = None
-            grasp_quality = None
-            distance_grasp_to_user_input_norm = None
+        if user_input_3d_dir is not None:
+            mean_evaluation_metric, grasp_quality, distance_grasp_to_user_input_norm = calc_evaluation_metrics(action, policy, user_input_point_2d, state.camera_intr)
+
             
         actions.append(action)
         states.append(state)
@@ -355,14 +362,25 @@ def run_dex_net(model_name,
         vis.title("Planned grasp at depth {0:.3f}m with Q={1:.3f}".format(
             action.grasp.depth, action.q_value))
         vis.show()
-    return mean_evaluation_metric, grasp_quality, distance_grasp_to_user_input_norm
+    
+    return mean_evaluation_metric, grasp_quality, distance_grasp_to_user_input_norm, action
 
+
+def get_object_name(depth_im_filename, n_level_above):
+    parent_dir = depth_im_filename
+    for i in range(n_level_above):
+        last_slash_index = parent_dir[::-1].index("/")
+        parent_dir = parent_dir[:-(last_slash_index+1)]
+    object_name = depth_im_filename.lstrip(parent_dir)
+    first_slash_index = object_name[::-1].index("/")
+    object_name = object_name[:-(first_slash_index+1)]
+    return object_name
 
 def calc_evaluation_metrics(action, policy, user_input_point_2d, camera_intr):
     grasp_quality = action.q_value
+    depth = 0.7 # [m]; Dex-Net is trained on objects that are 0.65-0.75m away
     distance_grasp_to_user_input_in_pixels = policy.calc_distance_grasp_to_user_input(action.grasp, user_input_point_2d)
     # transform from pixels to meters
-    depth = 0.7 # [m]; Dex-Net is trained on objects that are 0.65-0.75m away
     # transform from pixel coordinates to camera coordinates [m]
     distance_grasp_to_user_input_in_meters = distance_grasp_to_user_input_in_pixels * depth / camera_intr.K[1,1] 
     distance_threshold = 0.05 # [m] at which the normalized distance score is 0
@@ -371,7 +389,65 @@ def calc_evaluation_metrics(action, policy, user_input_point_2d, camera_intr):
         distance_grasp_to_user_input_norm = 0
     # calculate geometric mean as evaluation metric
     mean_evaluation_metric = np.sqrt(distance_grasp_to_user_input_norm * grasp_quality)
-    return mean_evaluation_metric, grasp_quality, distance_grasp_to_user_input_norm 
+    return mean_evaluation_metric, grasp_quality, distance_grasp_to_user_input_in_meters 
+
+def transform_grasp_to_synthetic_object_frame(grasp, camera_pose):
+    # transform the output grasp of dexnet from image to world camera frame. 
+    camera_pose_world = np.loadtxt(camera_pose)
+    # TODO: delete
+    # R = np.eye(4)
+    # R[:3,:3] = camera_pose_world[:3,:3]
+    # T = np.eye(4)
+    # T[:,-1] = camera_pose_world[:,-1]
+
+
+    # transform grasp center from image to camera frame 
+    K = np.concatenate((grasp.camera_intr.K, np.zeros((3,1))), axis=1)
+    K = np.concatenate((K, np.array([[0, 0, 0, 1]])), axis=0)
+    translation_grasp_cam = np.matmul(np.linalg.pinv(K), np.array([[grasp.depth*grasp.center.x], [grasp.depth*grasp.center.y], [grasp.depth*1], [1]]))
+    # take into account that z axis is pointing away from the object
+    translation_grasp_cam[2] = -translation_grasp_cam[2]
+
+    # # from package
+    # camera_model = image_geometry.PinholeCameraModel()
+    # camera_model.fromCameraInfo(self.camera_info)
+    # depth = 1.097 #1.16 
+
+    # v = camera_model.projectPixelTo3dRay((bb_cx, bb_cy))
+    # d_cam = np.concatenate( (depth*np.array(v), np.ones(1))).reshape((4, 1))
+
+
+
+
+    # # transform grasp orientation to camera frame 
+    # origin, xaxis, yaxis, zaxis = [0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]
+    # grasp_approach_axis = grasp.approach_axis
+    # xz_len = np.sqrt(grasp_approach_axis[1]**2 + grasp_approach_axis[2]**2)
+    # alpha = acos
+    # - np.arctan2(grasp_approach_axis[1],grasp_approach_axis[2])
+    # Rx = rotation_matrix(alpha, xaxis)
+    # grasp_approach_axis_transformed = matmul(Rx, grasp_approach_axis)
+    # beta = np.arctan2(grasp_approach_axis_transformed[0],grasp_approach_axis_transformed[2])
+    # Ry = rotation_matrix(beta, yaxis)
+    # rotation_grasp_cam = concatenate_matrices(Rx, Ry)
+    # grasp_in_cam_frame = np.concat(rotation_grasp_cam, translation_grasp_cam) #TODO: Add 0s
+
+
+    # grasp_cam = np.concatenate(grasp_cam, np.ones(1,1))
+    # grasp_world = np.matmul(np.linalg.inv(camera_pose_world), translation_grasp_cam)
+    grasp_world = np.matmul(camera_pose_world, translation_grasp_cam)
+
+
+    # calculate rotation matrix from approach axis (surface normal at grasp center) 
+    return grasp_world[:3]
+
+def save_grasp(grasp_world, camera_pose_path):
+    # set directory to save
+    last_slash_index = camera_pose_path[::-1].index("/")
+    parent_dir = camera_pose_path[:-(last_slash_index+1)]
+    file_name = "grasp" + camera_pose_path[-last_slash_index:]
+    file_path = parent_dir + file_name
+    np.savetxt(file_path, grasp_world)
 
 if __name__ == "__main__":
     # Parse args.
@@ -409,7 +485,7 @@ if __name__ == "__main__":
                         type=str,
                         default=None,
                         help="path to camera pose file to use")
-    parser.add_argument("--user_input_3d_folder",
+    parser.add_argument("--user_input_3d_dir",
                         type=str,
                         default=None,
                         help="path to folder containing user_input points to use")
@@ -436,11 +512,11 @@ if __name__ == "__main__":
     config_filename = args.config_filename
     fully_conv = args.fully_conv
     camera_pose_path = args.camera_pose_path
-    user_input_3d_folder = args.user_input_3d_folder
+    user_input_3d_dir = args.user_input_3d_dir
     user_input_fusion_method = args.user_input_fusion_method
     user_input_weight = args.user_input_weight
 
-    run_dex_net(model_name, 
+    mean_evaluation_metric, grasp_quality, distance_grasp_to_user_input_norm, action = run_dex_net(model_name, 
         depth_ims_dir, 
         depth_im_filename,
         segmask_filename, 
@@ -449,6 +525,11 @@ if __name__ == "__main__":
         config_filename, 
         fully_conv, 
         camera_pose_path,
-        user_input_3d_folder, 
+        user_input_3d_dir, 
         user_input_fusion_method, 
         user_input_weight)
+
+    grasp_world = transform_grasp_to_synthetic_object_frame(action.grasp, camera_pose_path)
+    print(grasp_world)
+    save_grasp(grasp_world, camera_pose_path)
+
